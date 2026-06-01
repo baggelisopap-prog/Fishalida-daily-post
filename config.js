db792984a61f637add59af2d54c8e8883032ce90
baggelisopap-prog <baggelisopap@gmail.com>
2026-06-01 14:19:07 +0300

    Update APP_CONFIG with N8N webhook URL
    Added N8N webhook URL to application configuration.

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 /**
  * Ρυθμίσεις Εφαρμογής & Supabase Credentials
  * * Προσοχή: Επειδή δεν υπάρχει build step (.env αρχεία), το anon key
@@ -6,5 +7,6 @@
  */
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://ygzdlbnaqtngwsnjcodo.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnemRsYm5hcXRuZ3dzbmpjb2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NjcxNDgsImV4cCI6MjA5NTQ0MzE0OH0.7mFPfCabi7TnpyzhUfHOXWyXGfEpnw-iCA2a3SZ4Pqk'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnemRsYm5hcXRuZ3dzbmpjb2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NjcxNDgsImV4cCI6MjA5NTQ0MzE0OH0.7mFPfCabi7TnpyzhUfHOXWyXGfEpnw-iCA2a3SZ4Pqk',
+  N8N_WEBHOOK_GENERATE_POST: 'http://localhost:5678/webhook-test/generate-post'
 };
